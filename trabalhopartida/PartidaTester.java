2fa9070ef4f529a4877a7fdf6feaf8a93e10a775
n-psilva <natanp.silva@hotmail.com>
2019-09-21 13:41:04 -0300

    Create PartidaTester.java

---
+++ trabalhopartida/PartidaTester.java
@@ -0,0 +1,51 @@
+
+package trabalhopartida;
+
+import java.util.Scanner;
+
+
+public class PartidaTester {
+
+    public static void main(String[] args) {
+       
+            Partida p1 = new Partida();
+
+	    Time l = new Time();      
+	    Time v = new Time();
+
+      	    Scanner leitura = new Scanner(System.in);
+            int aux;
+            
+		
+
+	        System.out.println("Time Local: ");
+		String t1 = leitura.next();
+		l.setNome(t1);
+		System.out.println("\nTime Visitante: ");
+		String t2 = leitura.next();
+		v.setNome(t2);   
+                
+            
+            do{
+		p1.iniciarPartida(l, v);
+		p1.mostraResultado();
+		p1.finalizarPartida();
+                
+                System.out.println("continuar partida?");
+                String resp = leitura.next();
+                if(!resp.equalsIgnoreCase("sim")){
+	         aux = 0;
+                 }
+                else
+                    aux = 1;
+            }while(aux == 1);
+
+		v.listar();
+
+		l.listar(); 
+
+		
+
+	 }
+  
+ }
