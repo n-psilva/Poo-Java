c163598f888804687e1370cf70ee47a5315ab91e
n-psilva <natanp.silva@hotmail.com>
2019-11-03 10:59:22 -0300

    Create IShapeTester.java

---
+++ trabalhosemana8/IShapeTester.java
@@ -0,0 +1,46 @@
+package trabalhoshape;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author natan
+ */
+public class IShapeTester {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Retangulo ret = new Retangulo();
+
+	Scanner ler = new Scanner(System.in); 
+        boolean aux = true;
+       
+        
+        do
+        {
+            
+        try
+        {
+            System.out.println("Entre com a base do retangulo: ");
+            float base = ler.nextFloat();
+            ret.setBase(base);
+            System.out.println("Entre com a altura do retangulo: ");
+            float alt = ler.nextFloat();
+            ret.setAltura(alt);
+            ret.imprimir();
+            aux = false;
+        }
+        catch(InputMismatchException e)
+           {
+               System.out.println("Digite apenas numeros!");
+               ler.nextLine();
+           }
+
+        }while( aux == true );
+       
+        
+    }
+    
+}
