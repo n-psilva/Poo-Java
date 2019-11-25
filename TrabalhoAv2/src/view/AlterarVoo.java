5e791320b563e9f7da44f1a2f65b04277598ebe4
n-psilva <natanp.silva@hotmail.com>
2019-11-25 17:38:37 -0300

    Update AlterarVoo.java

--- TrabalhoAv2/src/view/AlterarVoo.java
+++ TrabalhoAv2/src/view/AlterarVoo.java
@@ -44,7 +44,7 @@ public class AlterarVoo extends JFrame {
 	 */
 	public AlterarVoo() {
 		setResizable(false);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 220, 175);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
