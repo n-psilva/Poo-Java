228bbdd6b832ba475af7e4062889d7360c9a9935
n-psilva <natanp.silva@hotmail.com>
2019-11-25 17:40:10 -0300

    Update IncluirAero.java

--- TrabalhoAv2/src/view/IncluirAero.java
+++ TrabalhoAv2/src/view/IncluirAero.java
@@ -46,7 +46,7 @@ public class IncluirAero extends JFrame {
 	 */
 	public IncluirAero() {
 		setResizable(false);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 246, 221);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
