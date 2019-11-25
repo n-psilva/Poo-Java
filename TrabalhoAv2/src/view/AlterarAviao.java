8316daa35879dc1cd21bcf5cc9300d2e7d1338ac
n-psilva <natanp.silva@hotmail.com>
2019-11-25 17:38:16 -0300

    Update AlterarAviao.java

--- TrabalhoAv2/src/view/AlterarAviao.java
+++ TrabalhoAv2/src/view/AlterarAviao.java
@@ -44,7 +44,7 @@ public class AlterarAviao extends JFrame {
 	 */
 	public AlterarAviao() {
 		setResizable(false);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 215, 150);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
